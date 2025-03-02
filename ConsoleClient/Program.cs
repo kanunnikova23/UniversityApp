e1b18a01582d3fcd7e82df64fb4383cb15561961
aresceribilis <aresceribilis@gmail.com>
2025-03-02 16:07:02 +0200

    fix console encoding

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+Console.InputEncoding = Encoding.UTF8;
 
 Console.WriteLine("Hello, World!");
