c52834d1f36852bd5755b7c5bd60840628ce3005
aresceribilis <aresceribilis@gmail.com>
2025-03-02 16:10:25 +0200

    update Program

--- ConsoleClient/Program.cs
+++ ConsoleClient/Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
+using ConsoleClient;
 
 Console.OutputEncoding = Encoding.UTF8;
 Console.InputEncoding = Encoding.UTF8;
 
 Console.WriteLine("Hello, World!");
+
+var khnure = UniversityInitializer.Initialize();
+Console.WriteLine(khnure);
